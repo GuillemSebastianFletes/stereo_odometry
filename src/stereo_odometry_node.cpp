e20044bd4fb5d24081461ee2b1011faad862d7ba
guillemsebastianfletes <guisefle@gmail.com>
2015-06-02 11:39:25 +0200

    load all the images as a video

--- src/stereo_odometry_node.cpp
+++ src/stereo_odometry_node.cpp
@@ -21,8 +21,37 @@
 #include <math.h>
 //#include <std_msgs/Float64.h>
 
-using namespace message_filters;
+
 using namespace cv;
 using namespace std;
 
+int main( int argc, char** argv )
+{
+    char path[] = "/home/guillem/07/image_0/";
+    char format[] = ".png";
+    char name[50];
+    char name_old[50];
+    int n;
 
+    Mat actual_frame;
+    Mat previous_frame;
+
+    for (int i = 1; i < 1101; i++) //cargar la secuencia imagenes
+    {
+
+        n = sprintf(name,"%s%.6d%s", path, i, format);
+        n = sprintf(name_old,"%s%.6d%s",path,i-1,format);
+        cout << name << "   " << name_old << endl;
+
+
+        actual_frame = imread(name);
+        previous_frame = imread(name_old);
+        namedWindow( "actual", WINDOW_AUTOSIZE);
+        namedWindow( "old", WINDOW_AUTOSIZE);
+        imshow("actual", actual_frame);
+        imshow("old",previous_frame);
+        waitKey(10);
+    }
+
+    return 0;
+}
